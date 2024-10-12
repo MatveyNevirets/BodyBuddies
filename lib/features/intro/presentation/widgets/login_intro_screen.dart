331e59870a8b5d9738dccd6b9f2e15ac3b95565d
Matvey <matvey.works@gmail.com>
2024-10-12 16:24:10 +0400

    Revert "Edit: "Check second not appbar login version""
    This reverts commit e73b0eafd18ba6719aaac7bad0ca763bde8ae149.

--- lib/features/intro/presentation/widgets/login_intro_screen.dart
+++ lib/features/intro/presentation/widgets/login_intro_screen.dart
@@ -13,96 +13,97 @@ class LoginIntroScreen extends StatelessWidget {
     return Scaffold(
       body: Wrap(
         children: [
-          SafeArea(
-            child: Container(
-              margin: Styles.base_magrin_size,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                      child: Text(
-                    Strings.intro_login_title_text,
-                    style: Styles.title_text_style,
-                  )),
-                  const SizedBox(
-                    height: 130,
-                  ),
-                  Text(
-                    Strings.intro_login_enter_login_text,
-                    style: Styles.medium_text_style,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: loginController,
-                    cursorColor: Colours.app_bar_color.withAlpha(200),
-                    decoration: InputDecoration(
-                      hintText: Strings.intro_login_textfield_login_text,
-                      hintStyle: Styles.hint_text_style,
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 2,
-                          color: Colours.app_bar_color,
-                        ),
+          Container(
+            margin: Styles.base_magrin_size,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Center(
+                    child: Text(
+                  Strings.intro_login_title_text,
+                  style: Styles.title_text_style,
+                )),
+                const SizedBox(
+                  height: 70,
+                ),
+                Text(
+                  Strings.intro_login_enter_login_text,
+                  style: Styles.medium_text_style,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: loginController,
+                  cursorColor: Colours.app_bar_color.withAlpha(200),
+                  decoration: InputDecoration(
+                    hintText: Strings.intro_login_textfield_login_text,
+                    hintStyle: Styles.hint_text_style,
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Colours.app_bar_color,
                       ),
-                      border: const OutlineInputBorder(),
                     ),
+                    border: const OutlineInputBorder(),
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    Strings.intro_login_enter_password_text,
-                    style: Styles.medium_text_style,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    cursorColor: Colours.app_bar_color.withAlpha(200),
-                    decoration: InputDecoration(
-                      hintText: Strings.intro_login_textfield_password_text,
-                      hintStyle: Styles.hint_text_style,
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 2,
-                          color: Colours.app_bar_color,
-                        ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  Strings.intro_login_enter_password_text,
+                  style: Styles.medium_text_style,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  cursorColor: Colours.app_bar_color.withAlpha(200),
+                  decoration: InputDecoration(
+                    hintText: Strings.intro_login_textfield_password_text,
+                    hintStyle: Styles.hint_text_style,
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Colours.app_bar_color,
                       ),
-                      border: const OutlineInputBorder(),
                     ),
+                    border: const OutlineInputBorder(),
                   ),
-                  SizedBox(
-                    height: 50,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Center(
+                  child: Container(
+                    width: 250,
+                    child: BaseButton(
+                        onClick: () => print("Do Someth.."),
+                        buttonText: Strings.button_login_text,
+                        icon: null,
+                        isElevated: true),
                   ),
-                  Center(
-                    child: Container(
-                      width: 250,
-                      child: BaseButton(
-                          onClick: () => print("Do Someth.."),
-                          buttonText: Strings.button_login_text,
-                          icon: null,
-                          isElevated: true),
-                    ),
+                ),
+                SizedBox(height: 30,),
+                Center(
+                  child: Container(
+                    width: 250,
+                    child: BaseButton(
+                        onClick: () => print("Do Someth.."),
+                        buttonText: Strings.button_registration_text,
+                        icon: null,
+                        isElevated: false),
                   ),
-                  SizedBox(height: 30,),
-                  Center(
-                    child: Container(
-                      width: 250,
-                      child: BaseButton(
-                          onClick: () => print("Do Someth.."),
-                          buttonText: Strings.button_registration_text,
-                          icon: null,
-                          isElevated: false),
-                    ),
-                  ),
-                  SizedBox(height: 40,),
-                  Center(child: Text(Strings.intro_login_mini_info_text ,style: Styles.mini_info_text_style, textAlign: TextAlign.center,),),
-                ],
-              ),
+                ),
+                SizedBox(height: 40,),
+                Center(child: Text(Strings.intro_login_mini_info_text ,style: Styles.mini_info_text_style, textAlign: TextAlign.center,),),
+              ],
             ),
           ),
         ],
